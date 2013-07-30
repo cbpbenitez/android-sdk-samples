8c4b896502b1dbdf54438e592f25395b61f1fd2d
cbpbenitez <cbpbenitez@gmail.com>
2013-07-30 02:20:27 +0000

    - action bar sherlock sample

---
+++ abssample/src/org/kaithe/android/MainActivity.java
@@ -0,0 +1,33 @@
+package org.kaithe.android;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+
+import android.os.Bundle;
+
+public class MainActivity extends SherlockActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("Save")
+            .setIcon(R.drawable.ic_compose)
+            .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+
+        menu.add("Search")
+            .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+
+        menu.add("Refresh")
+            .setIcon(R.drawable.ic_refresh)
+            .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+
+        return true;
+	}
+
+}
